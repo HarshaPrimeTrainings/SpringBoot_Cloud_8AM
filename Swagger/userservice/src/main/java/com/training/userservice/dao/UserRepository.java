5f07ac929dd1dd73b61e8ae26cb2684cbf7959f9
HarshaVardhan <athaluri.harsha@gmail.com>
2023-05-19 08:55:31 +0530

    swagger

---
+++ Swagger/userservice/src/main/java/com/training/userservice/dao/UserRepository.java
@@ -0,0 +1,22 @@
+package com.training.userservice.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.training.userservice.model.User;
+
+@Repository
+public interface UserRepository extends PagingAndSortingRepository<User, Integer>{
+	
+	public User getByUname(String uname);
+	
+	public List<User> findByAddr(String addr);
+	
+	@Query(value= "select * from user where uname=:name and uid=:id",nativeQuery = true)
+	public User findDetailsBynameandId(@Param(value = "name") String name, @Param(value = "id") Integer id);
+}
